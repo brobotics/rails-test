a167f2ae33497a3d5e1327bab7d3e71d0d5e3424
Michael J. Boardman <boardman.michael@gmail.com>
2012-11-11 12:05:10 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catchup::Application.config.secret_token = 'a43f594c972396d178edb479e2ec95c879a7c7ffa418dd819aa1bab6f14012ad9fb3a407bcc6533f42945b8b251ce4825cd0537d3ca330cb1f41440411c7f406'
